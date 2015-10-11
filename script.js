1cd0d67ee1ad94467753fa9223b08d10687672d1
franxyzxyz <franxyzxyxz@gmail.com>
2015-10-11 16:52:36 +0800

    add prompt to get email

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // javascript to be added
+// $("#button-yellow").on("click",function(){
+//   console.log("tes");
+//   var email = prompt("Enter Your Email");
+// })
+window.onload = function(){
+  document.getElementById("button-yellow").addEventListener("click", function(){
+    var email = prompt("Enter Your Email");
+  })
+}
